45bf96d02523ff96dad2658b52bde592e5da04a1
Tere <mteresaarriola@gmail.com>
2026-07-30 22:12:36 -0300

    qa-control

--- bin/mixin-patch-cli.js
+++ bin/mixin-patch-cli.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 
-"use strict";
-
 import { patchProject } from '../dist/mixin-patch.js';
 
 patchProject(process.cwd()).catch(function(err){
     console.error(err);
-    process.exit(1);
+    process.exit(1);  /* eslint-disable-line no-process-exit */
 });
